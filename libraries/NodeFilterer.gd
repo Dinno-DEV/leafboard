fc3a3cdcc6aad8cdf22787bbc8b8e515ad41188a
DinnoDEV <109049468+Dinno-DEV@users.noreply.github.com>
2025-11-09 20:01:13 +0700

    NodeFilterer implementation updated

--- libraries/NodeFilterer.gd
+++ libraries/NodeFilterer.gd
@@ -21,5 +21,5 @@ static func by_name(nodes:Array[Node], node_name:String) -> Node:
 static func by_type(nodes:Array[Node], node_type:String) -> Array[Node]:
 	var filter_result:Array[Node] = []
 	for node in nodes:
-		if node.is_class(node_type): filter_result.append(node)
+		if NodeUtil.get_type_as_string(node) == node_type: filter_result.append(node)
 	return filter_result
