5446fab72f5be7d46e1eab598a5befc1d129dc23
DinnoDEV <109049468+Dinno-DEV@users.noreply.github.com>
2025-11-09 20:26:38 +0700

    Selector Implemented

---
+++ custom_resource_scripts/Selector.gd
@@ -0,0 +1,37 @@
+class_name Selector
+extends BoxContainer
+
+signal selected(id:String)
+
+func _ready() -> void:
+	set_all_button_toggle_mode()
+	disable_all_non_id_button()
+	var id_buttons:Array[Node] = NodeFilterer.by_type(get_children(), "IdButton")
+	for id_button in id_buttons:
+		id_button.pressed_id.connect(_on_id_button_pressed_id)
+
+func set_all_button_toggle_mode() -> void:
+	for node in get_children():
+		if node is Button:
+			node.toggle_mode = true
+
+func disable_all_non_id_button() -> void:
+	for node in get_children():
+		if node is not IdButton && node is Button:
+			node.disabled = true
+
+func unselect_all() -> void:
+	for node in get_children():
+		if node is Button:
+			node.button_pressed = false
+
+func select(id:String) -> void:
+	unselect_all()
+	for node in get_children():
+		if node is IdButton:
+			if node.button_id == id:
+				node.button_pressed = true
+
+func _on_id_button_pressed_id(id:String):
+	selected.emit(id)
+	select(id)
