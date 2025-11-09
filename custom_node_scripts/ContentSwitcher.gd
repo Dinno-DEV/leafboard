068bc05a17cca51693b22b1c3a0fd0fcece5020c
DinnoDEV <109049468+Dinno-DEV@users.noreply.github.com>
2025-11-09 20:44:37 +0700

    ControlSwitcher implementation updated

--- custom_node_scripts/ContentSwitcher.gd
+++ custom_node_scripts/ContentSwitcher.gd
@@ -6,7 +6,14 @@ extends Control
 
 func _ready() -> void:
 	hide_all_content()
+	show_first_content()
 	selector.selected.connect(_on_selector_selected)
+
+func show_first_content() -> void:
+	for node in get_children():
+		if node is Control:
+			node.visible = true
+			return
 
 func hide_all_content() -> void:
 	for node in get_children():
