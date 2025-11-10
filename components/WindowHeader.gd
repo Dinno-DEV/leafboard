745863f81aca9c1f4f90bb7cb3439d8db96b25d9
DinnoDEV <109049468+Dinno-DEV@users.noreply.github.com>
2025-11-10 19:36:15 +0700

    WindowHeader updated

--- components/WindowHeader.gd
+++ components/WindowHeader.gd
@@ -1,8 +1,13 @@
 class_name WindowHeader
 extends Control
 
+@export_group("Referenced Nodes")
+@export var main_scene_background:PanelContainer
+@export var window_mode_button:IdButton
+
 var is_mouse_in:bool = false
 var is_repositioning:bool = false
+var is_maximized:bool = false
 var click_position:Vector2
 
 func _process(_delta: float) -> void:
@@ -22,3 +27,21 @@ func _on_mouse_entered() -> void:
 
 func _on_mouse_exited() -> void:
 	is_mouse_in = false
+
+func _on_minimize_pressed_id(_id: String) -> void:
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MINIMIZED)
+
+func _on_window_mode_pressed_id(_id: String) -> void:
+	if is_maximized: is_maximized = false; DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	else: is_maximized = true; DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
+	if is_maximized and main_scene_background != null:
+		main_scene_background.theme_type_variation = "PanelContainerMainRad0"
+		if window_mode_button != null:
+			window_mode_button.icon = preload("res://assets/icons/Windowed.png")
+	else:
+		main_scene_background.theme_type_variation = "PanelContainerMainRad10"
+		if window_mode_button != null:
+			window_mode_button.icon = preload("res://assets/icons/Maximize.png")
+
+func _on_close_pressed_id(_id: String) -> void:
+	get_tree().quit()
