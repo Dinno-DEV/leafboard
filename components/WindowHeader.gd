0b11433669a003d5a2d33e3711dbf23b4e855eb4
DinnoDEV <109049468+Dinno-DEV@users.noreply.github.com>
2025-11-10 17:58:10 +0700

    WindowHeader updated

---
+++ components/WindowHeader.gd
@@ -0,0 +1,9 @@
+class_name WindowHeader
+extends Control
+
+
+func _on_mouse_entered() -> void:
+	pass # Replace with function body.
+
+func _on_mouse_exited() -> void:
+	pass # Replace with function body.
