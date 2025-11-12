fc17059db0ab9951b5319beeef1d407a6719f80e
DinnoDEV <109049468+Dinno-DEV@users.noreply.github.com>
2025-11-12 16:54:33 +0700

    ContentSwitcherContainer implemented
    Container version of content switcher.

---
+++ custom_node_scripts/ContentSwitcherContainer.gd
@@ -0,0 +1,32 @@
+class_name ContentSwitcherContainer
+extends PanelContainer
+
+@export_group("Referenced Nodes")
+@export var selector:Selector
+
+func _ready() -> void:
+	hide_all_content()
+	show_first_content()
+	if !selector: 
+		printerr("[ContentSwitcherContainer|", self.name,"] Selector has not been defined.")
+		return
+	selector.selected.connect(_on_selector_selected)
+
+func show_first_content() -> void:
+	for node in get_children():
+		if node is Control:
+			node.visible = true
+			return
+
+func hide_all_content() -> void:
+	for node in get_children():
+		if node is Control:
+			node.visible = false
+
+func show_content(id:String) -> void:
+	var filter_result:Node = NodeFilterer.by_name(get_children(), id)
+	if filter_result is Control: filter_result.visible = true
+
+func _on_selector_selected(id:String) -> void:
+	hide_all_content()
+	show_content(id)
