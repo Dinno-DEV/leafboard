99b2e154efbb7ed79be6a09eb940070c53150ef1
DinnoDEV <109049468+Dinno-DEV@users.noreply.github.com>
2025-11-18 09:50:29 +0700

    DialogTextInputPanel impelemented

---
+++ components/DialogTextInputPanel.gd
@@ -0,0 +1,29 @@
+class_name DialogTextInputPanel
+extends PanelContainer
+
+@export var place_holder_text:String = ""
+@export_group("Referenced Nodes")
+@export var confirm_button:Button
+@export var cancel_button:Button
+@export var line_edit:LineEdit
+
+signal response_received(response:String, is_confirmed:bool)
+
+func _ready() -> void:
+	confirm_button.pressed.connect(_on_confirm_button_pressed)
+	cancel_button.pressed.connect(_on_cancel_button_pressed)
+	if place_holder_text != "": setup_dialog(place_holder_text)
+
+func setup_dialog(message:String):
+	set_message(message)
+	confirm_button.disabled = false
+	cancel_button.disabled = false
+
+func set_message(message:String):
+	line_edit.placeholder_text = message
+
+func _on_confirm_button_pressed():
+	response_received.emit(line_edit.text, true)
+
+func _on_cancel_button_pressed():
+	response_received.emit(line_edit.text, false)
