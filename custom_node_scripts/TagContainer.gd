b273f4c3c4a73d8901d2cb73360806fcca8c79b8
DinnoDEV <109049468+Dinno-DEV@users.noreply.github.com>
2025-11-22 06:29:18 +0700

    Update TagContainer.gd

--- custom_node_scripts/TagContainer.gd
+++ custom_node_scripts/TagContainer.gd
@@ -1,6 +1,13 @@
 class_name TagContainer
 extends FlowContainer
 
+signal tag_added(new_tag:String)
+signal tag_removed(removed_tag:String)
+
+func _ready() -> void:
+	child_entered_tree.connect(_on_child_entered)
+	child_exiting_tree.connect(_on_child_exited)
+
 func get_all_tags() -> Array[String]:
 	var result:Array[String] = []
 	for child in get_children():
@@ -10,3 +17,9 @@ func get_all_tags() -> Array[String]:
 
 func has_tag(tag_name:String) -> bool:
 	return tag_name in get_all_tags()
+
+func _on_child_entered(node:Node) -> void:
+	if node is TagButton: tag_added.emit(node.tag_name)
+
+func _on_child_exited(node:Node) -> void:
+	if node is TagButton: tag_removed.emit(node.tag_name)
