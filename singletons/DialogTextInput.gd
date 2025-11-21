46830104bc73e87688f553dde25028c3f8cde299
DinnoDEV <109049468+Dinno-DEV@users.noreply.github.com>
2025-11-21 09:00:02 +0700

    Update DialogTextInput.gd

--- singletons/DialogTextInput.gd
+++ singletons/DialogTextInput.gd
@@ -9,14 +9,20 @@ signal response_received(response:String, is_confirmed:bool)
 func _ready() -> void:
 	response_received.connect(_on_response_received)
 
-func request_response(message:String) -> Array:
+func request_response(message:String, max_letter:int = -1) -> Array:
 	var text_input_panel:DialogTextInputPanel = preload("res://components/DialogTextInputPanel.tscn").instantiate()
 	text_input_panel.place_holder_text = message
+	if max_letter > -1:
+		text_input_panel.line_edit.max_length = max_letter
 	dialog_container.add_child(text_input_panel)
 	var response = await text_input_panel.response_received
 	text_input_panel.queue_free()
+	await text_input_panel.tree_exited
 	response_received.emit(response[0], response[1])
 	return response
+
+func is_empty() -> bool:
+	return dialog_container.get_child_count() == 0
 
 func focus_first_dialog() -> void:
 	# unsee all child and see first DialogConfirmPanel child
