62485ceb9874782ad9574b241a6e377a8bfa73ba
DinnoDEV <109049468+Dinno-DEV@users.noreply.github.com>
2025-11-21 13:29:59 +0700

    Update Soundboard.gd

--- custom_node_scripts/Soundboard.gd
+++ custom_node_scripts/Soundboard.gd
@@ -1,35 +1,64 @@
 class_name Soundboard
 extends HFlowContainer
 
-func add_new_button(id:String, sound:AudioStreamMP3) -> void:
-	return
+func add_new_button(id:String, sound_path:String) -> void:
+	var sound_button:SoundboardButton = preload("res://components/SoundboardButton.tscn").instantiate()
+	sound_button.set_button_data(id, sound_path, 50, [])
+	add_child(sound_button)
 
 func delete_button(id:String):
-	return
+	for child in get_children():
+		if child is SoundboardButton:
+			if child.get_button_name() == id:
+				child.queue_free()
 
 func get_soundboard_name() -> String:
 	return name
 
-func get_all_button_id() -> Array:
-	return []
+func get_all_button_id() -> Array[String]:
+	var result:Array[String] = []
+	for child in get_children():
+		if child is SoundboardButton:
+			result.append(child.get_button_name())
+	return result
 
-func get_button_volume(id:String) -> int:
-	return 0
+func get_button_volume(id:String) -> float:
+	return get_button_by_id(id).get_button_volume()
 
 func get_button_tags(id:String) -> Array[String]:
-	return []
+	return get_button_by_id(id).get_button_tags()
 
 func get_now_playing() -> Array[String]:
-	return []
+	var result:Array[String] = []
+	for child in get_children():
+		if child is SoundboardButton:
+			if child.is_playing(): result.append(child.get_button_name())
+	return result
 
-func get_button_by_id(id:String):
-	return id
+func get_button_by_id(id:String) -> SoundboardButton:
+	for child in get_children():
+		if child is SoundboardButton:
+			if child.get_button_name() == id: return child
+	return null
 
 func set_soundboard_name(new_name:String) -> void:
 	name = new_name
 
-func set_button_visible(id:String):
-	return id
+func set_button_visible(id:String) -> void:
+	get_button_by_id(id).visible = true
 
-func set_button_size() -> void:
-	pass
+func set_button_invisible(id:String) -> void:
+	get_button_by_id(id).visible = false
+
+func set_all_buttons_visible() -> void:
+	for child in get_children():
+		if child is SoundboardButton: child.visible = true
+
+func set_all_buttons_invisible() -> void:
+	for child in get_children():
+		if child is SoundboardButton: child.visible = false
+
+func set_all_buttons_size(new_size:Vector2) -> void:
+	for child in get_children():
+		if child is SoundboardButton:
+			child.initial_custom_minimum_size = new_size
