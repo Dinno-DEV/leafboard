b7f94e0ef3a2d32b1343b14e7eb54b14c56a6d13
DinnoDEV <109049468+Dinno-DEV@users.noreply.github.com>
2025-11-24 14:40:17 +0700

    Update SoundButtonDataCollector.gd

--- custom_node_scripts/app_memory_scripts/SoundButtonDataCollector.gd
+++ custom_node_scripts/app_memory_scripts/SoundButtonDataCollector.gd
@@ -12,13 +12,16 @@ func _on_button_deleted() -> void:
 	var grandparent = get_parent().get_parent()
 	if grandparent is not Soundboard: return
 	AppMemory.delete_audio_button(grandparent.get_soundboard_name(), get_parent().get_index())
+	Saver.save_soundboards_data()
 
 func _on_button_moved(new_index:int, previous_index:int) -> void:
 	var grandparent = get_parent().get_parent()
 	if grandparent is not Soundboard: return
 	AppMemory.move_audio_button(grandparent.get_soundboard_name(), previous_index, new_index)
+	Saver.save_soundboards_data()
 
 func _on_soundboard_button_edited(new_button_data:Dictionary) -> void:
 	var grandparent = get_parent().get_parent()
 	if grandparent is not Soundboard: return
 	AppMemory.set_audio_button(grandparent.get_soundboard_name(), new_button_data, get_parent().get_index())
+	Saver.save_soundboards_data()
