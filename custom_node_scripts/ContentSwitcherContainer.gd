d438b6fdb4ecd57c85ed0b8602c4f2e682363bf0
DinnoDEV <109049468+Dinno-DEV@users.noreply.github.com>
2025-11-24 17:19:57 +0700

    Update ContentSwitcherContainer.gd

--- custom_node_scripts/ContentSwitcherContainer.gd
+++ custom_node_scripts/ContentSwitcherContainer.gd
@@ -1,10 +1,14 @@
 class_name ContentSwitcherContainer
 extends PanelContainer
 
+@export var unload_unselected:bool = true
 @export_group("Referenced Nodes")
 @export var selector:Selector
 
+var stored_nodes:Array[Node] = []
+
 signal content_switched(new_selected_node:Node)
+signal content_shown(node:Node)
 
 func _ready() -> void:
 	hide_all_content()
@@ -24,10 +28,17 @@ func hide_all_content() -> void:
 	for node in get_children():
 		if node is Control:
 			node.visible = false
+			if unload_unselected:
+				stored_nodes.append(node)
+				remove_child(node)
 
 func show_content(id:String) -> void:
+	if unload_unselected:
+		for node in stored_nodes:
+			if node.name == id: add_child(node)
 	var filter_result:Node = NodeFilterer.by_name(get_children(), id)
 	if filter_result is Control: filter_result.visible = true
+	content_shown.emit(filter_result)
 
 func get_selected_node() -> Node:
 	for child in get_children():
