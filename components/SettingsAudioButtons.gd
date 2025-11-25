70101bf6b42ef40a91548e82502a7507d28fcc8a
DinnoDEV <109049468+Dinno-DEV@users.noreply.github.com>
2025-11-25 17:14:12 +0700

    Update SettingsAudioButtons.gd

--- components/SettingsAudioButtons.gd
+++ components/SettingsAudioButtons.gd
@@ -10,6 +10,7 @@ func _ready() -> void:
 	default_volume_slider.value_changed.connect(_on_value_changed)
 	default_name_line_edit.text_changed.connect(_on_text_changed)
 	default_tag_container.tag_added.connect(_on_tags_edited)
+	default_tag_container.tag_removed.connect(_on_tags_edited)
 
 func _on_value_changed(new_value:float) -> void:
 	AppMemory.set_audio_button_default_volume(new_value)
@@ -20,5 +21,6 @@ func _on_text_changed(new_text:String) -> void:
 	Saver.save_settings_data()
 
 func _on_tags_edited(_new_tag:String) -> void:
+	await get_tree().create_timer(0.1).timeout
 	AppMemory.set_audio_button_default_tag(default_tag_container.get_all_tags())
 	Saver.save_settings_data()
