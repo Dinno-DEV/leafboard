aa815b934530292333a2e4160bc39d750c06af8d
DinnoDEV <109049468+Dinno-DEV@users.noreply.github.com>
2025-11-25 13:16:18 +0700

    Added SettingsAudioButtonsLoader

--- components/SettingsAudioButtons.gd
+++ components/SettingsAudioButtons.gd
@@ -1,2 +1,24 @@
 class_name SettingsAudioButtons
 extends VBoxContainer
+
+@export_group("Referenced Nodes")
+@export var default_volume_slider:HSlider
+@export var default_name_line_edit:LineEdit
+@export var default_tag_container:TagContainer
+
+func _ready() -> void:
+	default_volume_slider.value_changed.connect(_on_value_changed)
+	default_name_line_edit.text_changed.connect(_on_text_changed)
+	default_tag_container.tag_added.connect(_on_tags_edited)
+
+func _on_value_changed(new_value:float) -> void:
+	AppMemory.set_audio_button_default_volume(new_value)
+	Saver.save_settings_data()
+
+func _on_text_changed(new_text:String) -> void:
+	AppMemory.set_audio_button_default_name(new_text)
+	Saver.save_settings_data()
+
+func _on_tags_edited(_new_tag:String) -> void:
+	AppMemory.set_audio_button_default_tag(default_tag_container.get_all_tags())
+	Saver.save_settings_data()
