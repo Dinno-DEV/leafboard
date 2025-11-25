7f655fc234c13e4f737ffcdb9b6d8f7892460fda
DinnoDEV <109049468+Dinno-DEV@users.noreply.github.com>
2025-11-25 19:12:29 +0700

    Updated InputRecorder

--- singletons/InputRecorder.gd
+++ singletons/InputRecorder.gd
@@ -1,1 +1,27 @@
 extends CanvasLayer
+
+@export_group("Referenced Nodes")
+@export var animation_player:AnimationPlayer
+
+signal input_received(event:InputEvent)
+
+func _input(event: InputEvent) -> void:
+	if event is not InputEventKey: return
+	var keycode:String = OS.get_keycode_string(event.get_keycode_with_modifiers())
+	if keycode.containsn("ctrl"): return
+	elif keycode.containsn("shift"): return
+	elif keycode.containsn("alt"): return
+	input_received.emit(event)
+	get_viewport().set_input_as_handled()
+
+func request_input_key() -> InputEvent:
+	return await input_received
+
+func reveal_dialog() -> void:
+	visible = true
+	animation_player.play("fade_in")
+
+func hide_dialog() -> void:
+	animation_player.play("fade_out")
+	await animation_player.animation_finished
+	visible = false
